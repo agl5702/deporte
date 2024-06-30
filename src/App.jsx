516d3857821196b063da7c6bdab2cebfd20178cc
agl5702 <advasquez41@misena.edu.co>
2024-06-29 19:27:33 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>hola</h1>
+      <h1>Bienvenido, disfruta de tu partido</h1>
+      <div className="iframe-container">
+        <iframe
+          src="https://www.capofut.net/play/premier5.php"
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+      </div>
     </>
   );
 }

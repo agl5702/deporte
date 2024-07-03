b5c104c46c65c8a776ae8caeaf20f79714f3cb7d
agl5702 <advasquez41@misena.edu.co>
2024-07-02 20:47:42 -0500

    add anuncios 3

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,13 @@ function App() {
       <div>
         <ExternalScript />
       </div>
+      <div>
+        <footer>
+          <p>
+            &copy; 2024 Ver Partidos en Vivo. Todos los derechos reservados.
+          </p>
+        </footer>
+      </div>
     </>
   );
 }
